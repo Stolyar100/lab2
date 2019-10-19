7f2bc275a9699119247d598661af1a81e8ab9b21
Stolyar100 <olejer555@gmail.com>
2019-10-19 16:58:41 +0300

    Still testing git

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!");
         }
     }
 }
